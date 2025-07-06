e5cf2fd10dc33f73669e15af49cd968742c68150
inworthitintern <inworthitintern@gmail.com>
2025-07-06 15:40:52 +0530

    changes

--- apps/isomorphic-starter/src/app/tenant/customers-vendors/vendor/create/page.tsx
+++ apps/isomorphic-starter/src/app/tenant/customers-vendors/vendor/create/page.tsx
@@ -1,21 +1,13 @@
-// "use client";
+"use client";
 
-// import CreateEditVendor from "@/app/shared/vendor/create-edit";
-// import PageHeader from "@/app/shared/page-header";
+import CreateEditVendor from "@/app/shared/vendor/create-edit";
+import PageHeader from "@/app/shared/page-header";
 
-// export default function CreateVendorPage() {
-//   return (
-//     <>
-//       <PageHeader title="Create Vendor" breadcrumb={[]}></PageHeader>
-//       <CreateEditVendor />
-//     </>
-//   );
-// }
-
-import React from "react";
-
-const page = () => {
-  return <div>page</div>;
-};
-
-export default page;
+export default function CreateVendorPage() {
+  return (
+    <>
+      <PageHeader title="Create Vendor" breadcrumb={[]}></PageHeader>
+      <CreateEditVendor />
+    </>
+  );
+}
--- apps/isomorphic-starter/src/app/tenant/customers-vendors/vendor/page.tsx
+++ apps/isomorphic-starter/src/app/tenant/customers-vendors/vendor/page.tsx
@@ -1,17 +1,25 @@
-"use client";
+// "use client";
 
-import Link from "next/link";
-import { PiPlusBold } from "react-icons/pi";
-import { Button } from "rizzui";
-import PageHeader from "@/app/shared/page-header";
-import VendorsTable from "@/app/shared/vendor/vendors-list/table";
-import { routesTenant } from "@/config/routes";
+// import Link from "next/link";
+// import { PiPlusBold } from "react-icons/pi";
+// import { Button } from "rizzui";
+// import PageHeader from "@/app/shared/page-header";
+// import VendorsTable from "@/app/shared/vendor/vendors-list/table";
+// import { routesTenant } from "@/config/routes";
 
-export default function VendorsPage() {
-  return (
-    <>
-      <PageHeader title="Vendors" breadcrumb={[]}></PageHeader>
-      <VendorsTable pageSize={20} />
-    </>
-  );
-}
+// export default function VendorsPage() {
+//   return (
+//     <>
+//       <PageHeader title="Vendors" breadcrumb={[]}></PageHeader>
+//       <VendorsTable pageSize={20} />
+//     </>
+//   );
+// }
+
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
